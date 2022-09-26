d127db31657959e84329f99a8b5f625ea45cb2e3
Ymvi <seanjohnson1236@gmail.com>
2022-09-26 07:39:22 +0000

    WEEK8

--- LogRocket/20220615-random/hardhat.config.js
+++ LogRocket/20220615-random/hardhat.config.js
@@ -11,33 +11,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
 module.exports = {
   solidity: "0.8.4",
-/*
+
   networks: {
-    "local-devnode": {
-       url: "http://localhost:8545",
-       accounts: { mnemonic: "test test test test test test test test test test test junk" }
-    },
-    "optimistic-kovan": {
-       url: "https://kovan.optimism.io",
-       accounts: { mnemonic: process.env.MNEMONIC }
-    },
-    "optimism": {
-       url: "https://mainnet.optimism.io",
-       accounts: { mnemonic: process.env.MNEMONIC }
+    "goerli": {
+       url: "https://eth-rinkeby.alchemyapi.io/v2/Vgkf_x1ZruUL5glIG--UkxnP6rvIpFtY",
+       accounts: ["3b28f3a35fddb7ca78edfd59a5eac444e0e3024516778243d48f5a9432c7936f"]
     }
   }
-*/
+
 };
